f00470cd6d424e5b033713d501ed2153cee9186b
Mike Fernandes <37893150+msferna1@users.noreply.github.com>
2018-08-06 14:50:54 +0200

    Update main.tf
    added variables

--- main.tf
+++ main.tf
@@ -1,14 +1,22 @@
+variable "region" {}
+variable "shared_credentials_file" {}
+variable "profile" {} 
+variable "key_name" {} 
+
 provider "aws" {
-  shared_credentials_file = "~/.aws/credentials/london"
-  region     = "eu-west-2"
-  profile    = "terraform"
+  shared_credentials_file = "${var.shared_credentials_file}"
+  region     = "${var.region}"
+  profile    = "${var.profile}"
 }
 
 resource "aws_instance" "test" {
   ami           = "ami-ee6a718a"
   instance_type = "t2.micro"
+  key_name   = "${var.key_name}"
   tags {
     Name = "tst-centos7"
+    Terraform = "true"
+    Environment = "Dev"
   }
 }
 
